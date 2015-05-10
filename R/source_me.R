762f434eb7dd34deef9d7926abe95c08e49d615c
fawda123 <mbafs2012@gmail.com>
2015-05-10 15:11:10 -0500

    tf16 and le13 processed for mean models

--- R/source_me.R
+++ R/source_me.R
@@ -7,9 +7,9 @@ data(pax_data)
 
 stats <- c('TF1.6', 'LE1.3')
 
-sal <- c(seq(1, 10, by = 2), 30)
-yrs <- c(seq(2, 13, by = 2), 50)
-mos <- c(seq(0.5, 1, by = 0.25), 1.5, 2, 10)
+sal <- c(seq(5, 15, by = 3), 30)
+yrs <- c(seq(5, 15, by = 3), 50)
+mos <- c(seq(0.5, 1, by = 0.25), 2, 10)
 grd <- expand.grid(sal, yrs, mos)
 names(grd) <- c('sal', 'yrs', 'mos')
 
@@ -37,11 +37,11 @@ for(stat in stats){
     wins <- with(wins, list(mos, yrs, sal))
     
     # fit model
-    mod <- WRTDStidal::modfit(tomod, wins = wins, tau = c(0.1, 0.5, 0.9), min_obs = FALSE, trace = T)
+    mod <- WRTDStidal::modfit(tomod, wins = wins, resp_type = 'mean', min_obs = FALSE, trace = T)
   
     nm <- paste(grd[i, 3], grd[i, 2], grd[i, 1], sep = '_')
     
-    nm <- paste0(stat, '_' , nm)
+    nm <- paste0(stat, 'mean_' , nm)
     assign(nm, mod)
     save(list = nm, file = paste0('data/', nm, '.RData'))
   

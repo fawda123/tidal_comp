47a34a7e360a538ba1c9e7a31c5ff10ff0093a5d
fawda123 <mbafs2012@gmail.com>
2015-05-22 11:35:13 -0500

    salinity to 0 - 1

--- R/source_me.R
+++ R/source_me.R
@@ -5,15 +5,15 @@ library(doParallel)
 
 data(pax_data)
 
-stats <- c('TF1.6', 'LE1.3')
+stats <- c('TF1.6', 'LE1.2')
 
-sal <- c(seq(5, 15, by = 3), 30)
+sal <- c(seq(0.1, 0.9, by = 0.3), 1)
 yrs <- c(seq(5, 15, by = 3), 50)
 mos <- c(seq(0.5, 1, by = 0.25), 2, 10)
 grd <- expand.grid(sal, yrs, mos)
 names(grd) <- c('sal', 'yrs', 'mos')
 
-cl <- makeCluster(6)
+cl <- makeCluster(7)
 registerDoParallel(cl)
 
 strt <- Sys.time()
@@ -36,16 +36,13 @@ for(stat in stats){
     # wts from grid
     wins <- grd[i, ]
     wins <- with(wins, list(mos, yrs, sal))
-    
+
     # fit model
-    mod <- WRTDStidal::modfit(tomod, wins = wins, resp_type = 'quantile', min_obs = FALSE, trace = T, tau = c(0.1, 0.5, 0.9))
-    # fit model
-    # mod <- WRTDStidal::modfit(tomod, wins = wins, resp_type = 'mean', min_obs = FALSE, trace = T)
+    mod <- WRTDStidal::modfit(tomod, wins = wins, resp_type = 'mean', min_obs = FALSE, trace = T)
     
+    # name and save the output
     nm <- paste(grd[i, 3], grd[i, 2], grd[i, 1], sep = '_')
-    
-    nm <- paste0(stat, '_' , nm)
-    # nm <- paste0(stat, 'mean_' , nm)
+    nm <- paste0(stat, 'mean_' , nm)
     assign(nm, mod)
     save(list = nm, file = paste0('data/', nm, '.RData'))
   

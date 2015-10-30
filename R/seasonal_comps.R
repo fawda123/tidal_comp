88ef71e9ee62db29ae85e5861cd57e539d354081
fawda123 <mbafs2012@gmail.com>
2015-10-30 09:03:23 -0500

    seasonal comp plot revise

--- R/seasonal_comps.R
+++ R/seasonal_comps.R
@@ -1,3 +1,31 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(wesanderson)
+library(scales)
+
+cols <- wes_palette('Zissou', 100, 'continuous') %>% 
+  as.character %>% 
+  .[1:60]
+
+theme_mine <- function (base_size = 12, base_family = "") {
+  theme_bw(base_size = base_size, base_family = base_family) %+replace% 
+  theme(
+    axis.text.x = element_text(size = 9),
+    axis.text.y = element_text(size = 9), 
+    plot.background = element_rect(fill='transparent', 
+      colour = NA),
+    panel.background = element_rect(fill='transparent', 
+      colour = NA),
+    legend.background = element_rect(fill='transparent', 
+      colour = NA),
+    strip.background = element_rect(fill = 
+        alpha(cols[length(cols)], 0.5)),
+    legend.key = element_rect(fill = 'transparent', 
+      colour = NA)
+    )   
+}
+
 data(bestLE12)
 data(bestTF16)
 
@@ -17,8 +45,11 @@ toplo <- rbind(bestLE12, bestTF16) %>%
   mutate(
     mod = factor(mod, levels = c('fits_gams', 'fits_wrtds', 'chla'), 
       labels = c('GAM', 'WRTDS', 'observed')),
-    yeargrp = cut(year_act, c(-Inf, 2000, Inf), c('1985-2000', '2001-2014'))
-  )
+    yeargrp = cut(year_act, c(-Inf, 1995, 2003, Inf), c('1985-1995', '1996-2003', '2004-2014'))
+  ) %>% 
+  filter(yeargrp != '1996-2003')
+
+
 
 ylab <- expression(paste('ln-Chl ',' (',italic('\u03bc'),'g ',L^-1,')'))
 xlab <- 'Day of year'

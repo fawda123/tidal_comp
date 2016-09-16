a9032f513966fa7ccc79efa5b1bc3baf8374ee7e
fawda123 <mbafs2012@gmail.com>
2016-09-16 12:49:27 -0500

    fixed issue with validation error in wrtds, working on optimization

--- R/validation.R
+++ R/validation.R
@@ -8,7 +8,7 @@ library(foreach)
 library(doParallel)
 source('text/R/funcs.R')
 
-# ######
+# ############
 # # find optimum window widths for WRTDS for whole weekly time series 
 # 
 # # parallel setup
@@ -19,9 +19,9 @@ source('text/R/funcs.R')
 # data(sims_day)
 # 
 # # format the daily data for weekly sampling and use with WRTDS optim
-# tomod <- select(sims_day, date, lnQ_sim, sim3) %>% 
+# tomod <- select(sims_day, date, sim1, lnQ_sim) %>% 
 #   rename(
-#     res = sim3,
+#     res = sim1,
 #     flo = lnQ_sim
 #     ) %>% 
 #   mutate(
@@ -37,7 +37,30 @@ source('text/R/funcs.R')
 # wk_opt <- winsrch_optim(tomod, upper = c(20, 100, 20), lower = c(0.25, 1, 0.25), 
 #   min_obs = FALSE, control = list(factr = 1e11))
 # 
-# save(wk_opt, file = 'data/wk_opt.RData', compress = 'xz')
+# # format the daily data for monthly sampling and use with WRTDS optim
+tomod <- select(sims_day, date, sim1, lnQ_sim) %>% 
+  rename(
+    res = sim1,
+    flo = lnQ_sim
+    ) %>% 
+  mutate(
+    lim = -1e6
+  ) %>% 
+  samp_sim(unit = 'month', irregular = F) %>% 
+  tidalmean(
+    reslab = expression(paste('ln-Chl-',italic(a),' (',italic('\u03bc'),'g ',L^-1,')')),
+    flolab = expression(paste('ln-Flow (', m^3, ' ', s^-1, ')'))
+  )
+# 
+# # run optimization
+# mo_opt <- winsrch_optim(tomod, upper = c(20, 100, 20), lower = c(0.25, 1, 0.25), 
+#   min_obs = FALSE, control = list(factr = 1e11))
+# 
+# # combine week optimization and month optimization
+# # load(file = 'data/wk_opt.RData')
+# val_opt <- list(wk_opt = wk_opt, mo_opt = mo_opt)
+# 
+# save(val_opt, file = 'data/val_opt.RData', compress = 'xz')
 
 ######
 # evaluation grid
@@ -45,7 +68,7 @@ source('text/R/funcs.R')
 
 unts <- c('week')
 mper <- seq(0.05, 0.5, by = 0.05)
-reps <- 1:10
+reps <- 1:20
 blck <- c(1, c(0.1, 0.5, 1))
 blckper <- c(F, T, T, T) # must be same length as blck
 grd <- expand.grid(blck, unts, mper, reps)
@@ -56,15 +79,15 @@ names(grd) <- c('blck', 'unts', 'mper', 'reps', 'blckper')
 # get WRTDS validation error
 
 # window widths data
-data(wk_opt)
-
+data(val_opt)
+wk_opt <- val_opt$wk_opt
 # simulated daily data
 data(sims_day)
 
 # format the daily data for use with WRTDS
-tosamp <- select(sims_day, date, lnQ_sim, sim3) %>% 
+tosamp <- select(sims_day, date, sim1, lnQ_sim) %>% 
   rename(
-    res = sim3,
+    res = sim1,
     flo = lnQ_sim
     ) %>% 
   mutate(
@@ -72,7 +95,7 @@ tosamp <- select(sims_day, date, lnQ_sim, sim3) %>%
   )
 
 # setup parallel
-cl <- makeCluster(7)
+cl <- makeCluster(5)
 registerDoParallel(cl)
 strt <- Sys.time()
 
@@ -115,13 +138,15 @@ wrtds_val <- foreach(val = 1:nrow(grd)) %dopar% {
     filter(date %in% valdat$date)
 
   # get error (wrtdsperf does not work 
-  errtrn <- wrtdsperf(trndat)[, 'rmse']
+  errtrn <- wrtdsperf(trn)[, 'rmse']
   errval <- rmse.fun(val$res.y, val$fits)
   
   # return errors
   c(errtrn, errval)
 
 }
+
+save(wrtds_val, file = 'data/wrtds_val.RData', compress = 'xz')
   
 ######
 # get GAMS validation error
@@ -130,9 +155,9 @@ wrtds_val <- foreach(val = 1:nrow(grd)) %dopar% {
 data(sims_day)
 
 # format the daily data for sub sampling and use with GAM
-tosamp <- select(sims_day, date, lnQ_sim, sim3) %>% 
+tosamp <- select(sims_day, date, sim1, lnQ_sim) %>% 
   rename(
-    res = sim3,
+    res = sim1,
     flo = lnQ_sim
     ) %>% 
   mutate(
@@ -142,7 +167,7 @@ tosamp <- select(sims_day, date, lnQ_sim, sim3) %>%
 tosamp$dec_time <- dec_time(tosamp$date)$dec_time
 
 # setup parallel
-cl <- makeCluster(7)
+cl <- makeCluster(5)
 registerDoParallel(cl)
 strt <- Sys.time()
 
@@ -172,18 +197,46 @@ gams_val <- foreach(val = 1:nrow(grd)) %dopar% {
   valdat <- alldat[samps$smps, ]
 
   # gam mod and predictions, rmse on validation  
-  trnmod <- try({gam(res~te(dec_time, doy, flo, bs=c("tp","cc","tp")), data = trndat, knots=list(doy=c(1,366)))})
-  if(inherits(trnmod, 'try-error')) next
-  valprd <- predict(trnmod, newdata = valdat)
-  errtrn <- rmse.fun(resid(trnmod))
-  errval <- rmse.fun(valdat$res, valprd)
+  trnmod <- try({
+    gam(res~te(dec_time, doy, flo, bs=c("tp","cc","tp")), k = c(5, 11, 5), data = trndat, knots=list(doy=c(1,366)))
+    })
+  if(inherits(trnmod, 'try-error')){
+    
+    errtrn <- errval <- NA
+    
+  } else {
+    
+    valprd <- predict(trnmod, newdata = valdat)
+    errtrn <- rmse.fun(resid(trnmod))
+    errval <- rmse.fun(valdat$res, valprd)
+    
+  }
   
   # output
   c(errtrn, errval)
   
 }
 
-toplo <- mutate(grd, 
+save(gams_val, file = 'data/gams_val.RData', compress = 'xz')
+
+######
+# plotting
+
+toplo <- data.frame(
+    mod = 'GAM', 
+    do.call('rbind', gams_val), 
+    grd
+  ) %>%
+  rbind(data.frame(
+    mod = 'WRTDS', 
+    do.call('rbind', wrtds_val), 
+    grd
+  )) %>% 
+  rename(
+    Training = X1, 
+    Validation = X2
+  ) %>% 
+  mutate(
     blckper = ifelse(blckper, 'prop', 'rand')
   ) %>%
   unite('blck', blck, blckper) %>% 
@@ -198,16 +251,12 @@ toplo <- mutate(grd,
       labels = c('Weeks')
     )
   ) %>%
-  rename(
-    Training = rmse_trn, 
-    Validation = rmse_val
-  ) %>% 
   gather('var', 'val', Training:Validation) %>% 
-  group_by(blck, unts, var, mper) %>% 
+  group_by(mod, blck, unts, var, mper) %>% 
   summarise(
     mean_val = median(val, na.rm = TRUE), 
-    max_val = quantile(val, 0.95), #max(val, na.rm = TRUE), 
-    min_val = quantile(val, 0.05)#min(val, na.rm = TRUE)
+    max_val = quantile(val, 0.95, na.rm = TRUE), #max(val, na.rm = TRUE), 
+    min_val = quantile(val, 0.05, na.rm = TRUE)#min(val, na.rm = TRUE)
   ) %>% 
   rename(
     `Block sizes` = blck
@@ -229,12 +278,12 @@ p <- ggplot(toplo, aes(x = mper, y = mean_val, group = `Block sizes`, colour = `
   geom_point(position = position_dodge(width = ddge_wd), size = 2) +
   geom_line(position = position_dodge(width = ddge_wd)) +
   geom_errorbar(aes(ymin = min_val, ymax = max_val), width = 0, position = position_dodge(width = ddge_wd)) +
-  facet_grid(~var, scales = 'free_y') +
+  facet_grid(var ~ mod, scales = 'free_y') +
   mytheme + 
   scale_x_discrete('Proportion of observations used as validation') +
   scale_y_continuous('RMSE')
 
-pdf('C:/Users/mbeck/Desktop/GAM_validation.pdf', height = 4, width = 8, family = 'serif')
+pdf('C:/Users/mbeck/Desktop/validation.pdf', height = 8, width = 8, family = 'serif')
 print(p)
 dev.off()
 
@@ -273,9 +322,9 @@ dev.off()
 #   
 #   titleval <- paste(unitlab, msslab, blcklab, sep = ', ')
 #   
-#   plot(sim3 ~ date, tmp$alldat, xlab = '', ylab = '')
+#   plot(sim1 ~ date, tmp$alldat, xlab = '', ylab = '')
 #   mtext(titleval, side = 3, cex = 0.8)
-#   points(sim3 ~ date, tmp$alldat[tmp$smps, ], col = 'red', pch = 19)
+#   points(sim1 ~ date, tmp$alldat[tmp$smps, ], col = 'red', pch = 19)
 #   
 # }
 
